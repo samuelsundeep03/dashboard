da7bbd701bb9658639aa016db33a70940dd90554
samuelsundeep03 <164985957+samuelsundeep03@users.noreply.github.com>
2024-07-26 12:34:45 +0530

    changes to the base file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Row>
-          <Col>
-            
-          </Col>
-        </Row>
       </body>
     </html>
   );
